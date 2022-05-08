19f6e2add6acb5a068c36b51911029ac890bc54c
Süha Kesikbaş <suha.kesikbas@outlook.com>
2022-05-08 19:40:02 +0300

    Ödev 2 Eklendi

---
+++ Odev2/index.js
@@ -0,0 +1,31 @@
+const blogs = [
+  { title: "Başlık 1", description: "Açıklama 1" },
+  { title: "Başlık 2", description: "Açıklama 2" },
+  { title: "Başlık 3", description: "Açıklama 3" },
+];
+
+const blogList = () => {
+  blogs.map((blog) => {
+    console.log(`Blog Başlığı : ${blog.title}, \nBlog Açıklaması : ${blog.description}\n`);
+  });
+};
+blogList();
+
+const addBlog = (blog) => {
+  const promise1 = new Promise((resolve, reject) => {
+    blogs.push(blog);
+    resolve(blogs);
+    reject("Blog eklenirken bir hata oluştu.");
+  });
+  return promise1;
+};
+
+async function showBlog() {
+  try {
+    await addBlog({ title: "Başlık 4", description: "Açıklama 4" });
+    blogList();
+  } catch (error) {
+    console.log(error);
+  }
+}
+showBlog();
