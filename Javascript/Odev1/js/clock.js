21243875e1150a3f716c45ad48e88a838a3b4138
Süha Kesikbaş <suha.kesikbas@outlook.com>
2022-06-05 01:15:19 +0300

    Added Javascript examples

---
+++ Javascript/Odev1/js/clock.js
@@ -0,0 +1,7 @@
+const today = new Date();
+document.getElementById("myClock").innerHTML = today.toDateString()+" tarihinde";
+
+let person = prompt("Lütfen adınızı giriniz.", "John Doe");
+if (person != null) {
+    myName.innerHTML=person;
+  }
